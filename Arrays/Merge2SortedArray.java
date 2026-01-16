58f6ccb1888d0458eec57bbd041527a63a7cc39d
roshita <roshlogin@gmail.com>
2026-01-16 21:19:57 +0530

    more code hehe

--- Arrays/Merge2SortedArray.java
+++ Arrays/Merge2SortedArray.java
@@ -30,7 +30,7 @@ public class Merge2SortedArray {
         int[] nums1 = {1,2,3,0,0,0};
         int[] nums2 = {2,5,6};
         int m = 3, n = 3;
-
+        
         merge(nums1, m, nums2, n);
         System.out.println(Arrays.toString(nums1));
     }
