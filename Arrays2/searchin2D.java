07b13575a9547b3a93f7c732559de70a9f8c709b
roshita <roshlogin@gmail.com>
2026-01-30 03:27:04 +0530

    this is the code to search element in 2D array

---
+++ Arrays2/searchin2D.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+
+
+public class searchin2D {
+
+    public static boolean search (int matrix[][],int key){
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]== key){
+                    System.out.println("element fount at cell["+i+","+j+"]");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int matrix [][] = new int[3][3];
+        int n=3, m=3;
+
+        //input loop
+        Scanner sc = new Scanner(System.in);
+        for (int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                matrix[i][j]= sc.nextInt();
+            }
+        }
+        //output loop
+        for (int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(matrix[i][j]+"   ");
+            }
+            System.out.println();
+        }
+        search(matrix, 6);
+    }
+}
