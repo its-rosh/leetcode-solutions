33820bd87953a99aeda93409c267b7ecd616c22c
roshita <roshlogin@gmail.com>
2026-02-02 04:51:30 +0530

    little changes

--- Arrays/diagonalSum.java
+++ Arrays/diagonalSum.java
@@ -4,6 +4,18 @@ public class diagonalSum {
         int sum = 0;
         int n = matrix.length;
 
+    //  for (int i =0;i<matrix.length;i++){
+    //         for(int j=0;j<matrix[0].length;j++){                                 //brute force sloution
+    //             if (i==j){
+    //                 sum += matrix[i][j];
+    //             }
+    //             else if(i+j==matrix.length-1){
+    //                 sum += matrix[i][j];    
+    //             }
+
+    //     }
+    // }
+    // return sum;
         for (int i = 0; i < n; i++) {
             // primary diagonal
             sum += matrix[i][i];
@@ -17,10 +29,7 @@ public class diagonalSum {
     }
 
     public static void main(String[] args) {
-        int matrix[][] = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
+        int matrix[][] = {{1, 2, 3},{4, 5, 6},{7, 8, 9}
         };
 
         System.out.println("Diagonal Sum = " + diagonalSum(matrix));
