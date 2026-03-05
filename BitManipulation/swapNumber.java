ad756f7e567fd9cc0230d3cd367311086ecfeb6a
roshita <roshlogin@gmail.com>
2026-03-06 03:40:12 +0530

    Swap Numbers using bit

--- BitManipulation/swapNumber.java
+++ BitManipulation/swapNumber.java
@@ -7,9 +7,9 @@ public class swapNumber {
         System.out.println("Before swap: x = " + x + " and y = " + y);
 
         // Swap using XOR
-        x = x ^ y;
-        y = x ^ y;
-        x = x ^ y;
+        x = x ^ y; // 3^4 0011 ^ 0100 = now x is 0111
+        y = x ^ y; // 0111 ^ 0100 = now y is 0011
+        x = x ^ y; // 0111^ 0011 = now x is 0100
 
         System.out.println("After swap: x = " + x + " and y = " + y);
     }
