bcdc2a74c9d835067b4843144a5657f6c35cb3bc
roshita <roshlogin@gmail.com>
2026-03-07 23:37:55 +0530

    start of patterns

---
+++ 1.Patterns/star.java
@@ -0,0 +1,13 @@
+public class star{
+    public static void main(String[] args) {
+        int n = 4;
+
+        for(int i= 1;i<=n;i++){
+            for(int star=1;star<=n;star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
