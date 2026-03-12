a7e869cfc5315e5d326c413d6277ef413ef049bb
roshita <roshlogin@gmail.com>
2026-03-12 23:44:48 +0530

    we can print Alphabets

---
+++ 1.Patterns/charPattern.java
@@ -0,0 +1,14 @@
+public class charPattern{
+    public static void main(String [] args){
+        char ch = 'A';
+        int n = 5;
+
+        for(int i =1; i<=n; i++){
+            for (int j =1; j<=i; j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
