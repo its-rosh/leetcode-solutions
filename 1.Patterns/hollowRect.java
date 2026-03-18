72bb783051cbeb60cac9578acc6592924c6f4d36
roshita <roshlogin@gmail.com>
2026-03-18 14:18:47 +0530

    new

--- 1.Patterns/hollowRect.java
+++ 1.Patterns/hollowRect.java
@@ -1,14 +1,14 @@
 public class hollowRect{
-    public static void hollow_rectangle(int totRows, int totCols){
-        for (int i=1; i<=totRows; i++){
-            for(int j=1;j<=totCols;j++){
-                if(i ==1 || i==totRows || j==1 || j== totCols){
+    public static int hollow_rectangle( int totalCol, int totalRow){
+        for(int i =1; i<=totalRow;i++){
+            for(int j=1;j<=totalCol;j++){
+                if(i==1 || j==1 || i==totalRow || j==totalCol){
                     System.out.print("*");
                 }else{
                     System.out.print(" ");
                 }
             }System.out.println();
-        }
+        }return 0;
     }
     public static void main(String []arg){
         hollow_rectangle(7, 7);
