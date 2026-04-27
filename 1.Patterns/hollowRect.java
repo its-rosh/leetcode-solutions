7ba7d9ee1993c2286e9debf682b2e79506ad7643
roshita <roshlogin@gmail.com>
2026-04-27 07:21:54 +0530

    hollow rectangle

--- 1.Patterns/hollowRect.java
+++ 1.Patterns/hollowRect.java
@@ -1,16 +1,17 @@
 public class hollowRect{
-    public static int hollow_rectangle( int totalCol, int totalRow){
-        for(int i =1; i<=totalRow;i++){
-            for(int j=1;j<=totalCol;j++){
-                if(i==1 || j==1 || i==totalRow || j==totalCol){
+
+    public static void rect( int totRow, int totCol){
+        for (int i=1;i<=totRow;i++){
+            for(int j=1; j<=totCol; j++){
+                if(i==1 || i==totRow || j==1 || j==totCol){
                     System.out.print("*");
                 }else{
                     System.out.print(" ");
                 }
             }System.out.println();
-        }return 0;
+        } 
     }
-    public static void main(String []arg){
-        hollow_rectangle(7, 7);
+    public static void main(String[] args) {
+        rect(4,4);
     }
 }
