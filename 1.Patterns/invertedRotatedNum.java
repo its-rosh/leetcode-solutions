0b62d08c45e6c15b2642d624565d870fb11dec56
roshita <roshlogin@gmail.com>
2026-05-06 17:24:17 +0530

    invert the numbers

---
+++ 1.Patterns/invertedRotatedNum.java
@@ -0,0 +1,14 @@
+public class invertedRotatedNum {
+    public static void invertedRotatedPyramidNum(int n ){
+
+    for (int i = 1;i<= n; i++){
+        for (int j=1; j<=n-i+1; j++){
+            System.out.print(j);
+        }
+        System.out.println();
+    }
+    }
+    public static  void main(String[] args){
+        invertedRotatedPyramidNum(9);
+    }
+}
