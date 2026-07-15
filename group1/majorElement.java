33f1d59df8b6b421626369c36228e8b9e7a9145d
roshita <roshlogin@gmail.com>
2026-07-15 23:20:26 +0530

    more code finally posted tody

---
+++ group1/majorElement.java
@@ -0,0 +1,12 @@
+// find majority element - element which occurs more then n/2 times ( n = total number of elements in array)
+// take arry -- sourt that arry -- elements will pass form the middle 
+// using hashtable -- make use of addctional data -- for every element -- if already exists increment the frequency if not add element and frequency as 1
+// more vote  
+
+
+public class majorElement{
+    public static void main(String[] args) {
+        int arry [] = [1,3,4,5,7,8,4];
+
+    }
+}
