b3c4bd95efccc43ed479d5b14c36c06ad87777f3
roshita <roshlogin@gmail.com>
2026-08-23 23:54:18 +0530

    finding single element out of arr

---
+++ array1/single_element.java
@@ -0,0 +1,20 @@
+public class SingleElement {
+
+    public static int singleNumber(int[] nums) {
+
+        int ans = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            ans = ans ^ nums[i];
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {2, 2, 1,3,3,1,4};
+
+        System.out.println(singleNumber(arr));
+    }
+}
